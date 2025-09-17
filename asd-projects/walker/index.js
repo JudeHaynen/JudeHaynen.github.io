a588294509f767fc1be850a464807e98401acd4c
JudeHaynen <judehaynen@gmail.com>
2025-09-17 14:49:01 +0000

    done

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -12,11 +12,21 @@ function runProgram(){
     UP: 38,
     RIGHT: 39,
     DOWN: 40,
+    W: 87,
+    A: 65,
+    S: 83,
+    D: 68,
   };
 
    var walker = {
     x : 0,
     y : 0,
+    speedX : 0,
+    speedY : 0,
+   }
+    var walker2 = {
+    x : 50,
+    y : 50,
     speedX : 0,
     speedY : 0,
    }
@@ -38,6 +48,8 @@ function runProgram(){
   */
   $(document).on('keydown', handleKeyDown);  
   $(document).on('keyup', handleKeyUp);                        
+  $(document).on('keydown', handleKeyDown2);  
+  $(document).on('keyup', handleKeyUp2);                        
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -49,8 +61,11 @@ function runProgram(){
   */
   function newFrame() {
     repositionGameItem();
+    repositionGameItem2();
     wallCollision();
+    wallCollision2();
     redrawGameItem();
+    redrawGameItem2();
     console.log(walker.x, walker.y);
   }
   
@@ -72,6 +87,18 @@ function runProgram(){
      walker.speedY = 5;
 }
   }
+  function handleKeyDown2(event) {
+    console.log(event.which);
+    if (event.which === KEY.A) {
+      walker2.speedX = -5;
+}if (event.which === KEY.D) {
+   walker2.speedX = 5;
+}if (event.which === KEY.W) {
+    walker2.speedY = -5;
+}if (event.which === KEY.S) {
+     walker2.speedY = 5;
+}
+  }
 function handleKeyUp(event) {
     console.log(event.which);
     if (event.which === KEY.LEFT) {
@@ -84,6 +111,18 @@ function handleKeyUp(event) {
      walker.speedY = 0;
 }
   }
+  function handleKeyUp2(event) {
+    console.log(event.which);
+    if (event.which === KEY.A) {
+      walker2.speedX = 0;
+}if (event.which === KEY.D) {
+   walker2.speedX = 0;
+}if (event.which === KEY.W) {
+    walker2.speedY = 0;
+}if (event.which === KEY.S) {
+     walker2.speedY = 0;
+}
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -94,7 +133,12 @@ function handleKeyUp(event) {
       walker.y -= walker.speedY;
     }
   } 
-
+ function wallCollision2() {
+    if (walker2.x < 0 || walker2.y < 0 || walker2.x + $("#walker").width() > $("#board").width() || walker2.y + $("#walker").height() > $("#board").height()) {
+      walker2.x -= walker2.speedX;
+      walker2.y -= walker2.speedY;
+    }
+  } 
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -110,4 +154,57 @@ function handleKeyUp(event) {
     $("#walker").css("left", walker.x);
     $("#walker").css("top", walker.y);
    }
+    function repositionGameItem2() {
+    walker2.x += walker2.speedX
+    walker2.y += walker2.speedY
+   }
+   function redrawGameItem2() {
+    $("#walker2").css("left", walker2.x);
+    $("#walker2").css("top", walker2.y);
+   }
+    $("#walker").on("click", changeColor);
+    
+    function changeColor(){
+    var color = Math.ceil(Math.random() * 6);
+    if (color === 1){
+      $("#walker").css("background-color", "yellow")
+    }
+    else if (color === 2){
+      $("#walker").css("background-color", "red")
+    }
+    else if (color === 3){
+      $("#walker").css("background-color", "blue")
+    }
+    else if (color === 4){
+      $("#walker").css("background-color", "green")
+    }
+    else if (color === 5){
+      $("#walker").css("background-color", "purple")
+    }
+    else if (color === 6){
+      $("#walker").css("background-color", "orange")
+    }
+  }
+  $("#walker2").on("click", changeColor2);
+    function changeColor2(){
+    var color = Math.ceil(Math.random() * 6);
+    if (color === 1){
+      $("#walker2").css("background-color", "yellow")
+    }
+    else if (color === 2){
+      $("#walker2").css("background-color", "red")
+    }
+    else if (color === 3){
+      $("#walker2").css("background-color", "blue")
+    }
+    else if (color === 4){
+      $("#walker2").css("background-color", "green")
+    }
+    else if (color === 5){
+      $("#walker2").css("background-color", "purple")
+    }
+    else if (color === 6){
+      $("#walker2").css("background-color", "orange")
+    }
+  }
 }
